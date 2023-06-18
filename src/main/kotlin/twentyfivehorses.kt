99b23fda0f731703a2305b0b48af0a45d7f58514
Christian Fuchs <kontaktierbar@gmail.com>
2023-06-18 12:12:44 +0200

    developing problem model

--- src/main/kotlin/twentyfivehorses.kt
+++ src/main/kotlin/twentyfivehorses.kt
@@ -1,3 +1,39 @@
+
+/** Assign each horse a unique ordinal number. */
+typealias Horse = Int
+
+/** Know which horses have slower or faster. */
+data class Score(
+    val fasterThan: Set<Horse>,
+    val slowerThan: Set<Horse>
+)
+
+/** State consists of the score of each horse. */
+data class State(
+    val scores: Array<Score>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as State
+        return scores.contentEquals(other.scores)
+    }
+
+    override fun hashCode(): Int {
+        return scores.contentHashCode()
+    }
+}
+
+/** Order of a subset of horses. */
+typealias RaceResult = Array<Horse>
+
+fun update(
+    state: State,
+    raceResult: RaceResult
+): State {
+    TODO("not implemented")
+}
+
 fun main() {
     println("hello world")
 }
